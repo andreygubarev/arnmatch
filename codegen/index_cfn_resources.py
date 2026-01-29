0a79d9692f20521c05f2c2b559d91906a9ef623f
Andrey Gubarev <andrey@andreygubarev.com>
2026-01-29 12:34:32 +0400

    add cfn resource indexer class for arn to cfn type mapping

---
+++ codegen/index_cfn_resources.py
@@ -0,0 +1,9 @@
+"""Maps ARN resource types to CloudFormation resource types."""
+
+
+class CFNResourceIndexer:
+    """Maps ARN resources to CloudFormation resource types."""
+
+    def process(self, cfn_mapping):
+        """Validate CFN resource mappings."""
+        pass
